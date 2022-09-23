03e50015121ea7d0bf927391984c62376c14bfef
simonb-rmit <s8401236@student.rmit.edu.au>
2022-09-23 14:05:40 +1000

    WIP
    WIP

---
+++ CropRecommender.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import numpy as np
+
+from sklearn.metrics.pairwise import cosine_similarity
+from sklearn import preprocessing
+
+df = pd.read_csv(r"CropProps.csv")
+
+
+
+rainfallFeatures = ['RainfallLow', 'RainfallMed', 'RainfallHigh', 'Winter']
+for f in rainfallFeatures:
+    df[f] = df[f].fillna(False)
+
+le = preprocessing.LabelEncoder()
+leM = le.fit(df['Maturity'])
+
+crops = df[['RainfallLow', 'RainfallMed', 'RainfallHigh', 'Winter']]
+crops['Maturity'] = leM.transform(df['Maturity'])
+
+cosine_sim = cosine_similarity(crops)
+
+print (cosine_sim)
